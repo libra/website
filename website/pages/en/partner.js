dbbefa332edfd3b059920c80069c084ce3ef6032
Eric Nakagawa <ericnakagawa@fb.com>
2019-06-05 07:11:38 -0700

    added two stubs for forms

---
+++ website/pages/en/partner.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const Container = CompLibrary.Container;
+
+function Help(props) {
+  const {config: siteConfig, language = ''} = props;
+  const {baseUrl, docsUrl} = siteConfig;
+  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+  const langPart = `${language ? `${language}/` : ''}`;
+  const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+  return (
+    <div className="docMainWrapper wrapper">
+      <Container className="mainContainer documentContainer postContainer">
+        <div className="post">
+          <header className="postHeader">
+            <h1>Sign up to learn more about running a node.</h1>
+          </header>
+          <form>
+            <input type="text" name="email" />
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+module.exports = Help;
