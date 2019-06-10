bcad4daf315055deb368741ad47e641bd3af7c21
Joshua Williams <joshua@dfuzr.com>
2019-06-10 11:10:57 -0700

    Added custom fonts for headers

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -75,7 +75,10 @@ const siteConfig = {
   ],
 
   // CSS sources to load
-  stylesheets: [`${baseUrl}css/code_block_buttons.css`],
+  stylesheets: [
+    `${baseUrl}css/code_block_buttons.css`,
+    `${baseUrl}css/fonts/NBInternationalProEditionWeb/stylesheet.css`,
+  ],
 
   // Custom markdown functions
   markdownPlugins: markdownPlugins,
